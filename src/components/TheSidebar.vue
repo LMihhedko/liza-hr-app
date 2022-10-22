010561fe6fb004335c4bdb51d8deb3a606bd9722
Mihhedkol <mihhdekol@gmail.com>
2022-10-22 16:04:01 +0200

    dummy commit 3

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col h-screen bg-500-red">
-    <img class="h-16 mb-6" src="../assets/company_logo.png" />
+    <img class="h-16 mb-6" src="../assets/company-logo.png" />
     <div class="sidebar-item flex">
       <img src="../assets/user-sidebar-icon.svg" class="w-4 mr-4" />Account
     </div>
